97e13bbf9bc9943bf702f9714b0707463797123c
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-07-17 10:06:14 +0200

    add e2e

---
+++ e2e-tests/steps/steps.js
@@ -0,0 +1,8 @@
+const { client } = require("nightwatch-api");
+const { Given } = require("cucumber");
+
+Given(/^I open the Address Manager home page$/, async () => {
+  const businesspartner = client.page.businesspartner()
+  console.info (businesspartner.url())
+  await businesspartner.navigate().waitForElementVisible('@body')
+})
